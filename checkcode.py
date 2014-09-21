0997fcf96f956a900b00892b6b30184b499f9136
vyshah <vyom.pablo@gmail.com>
2014-09-21 11:23:04 -0700

    Minor fixes.

--- checkcode.py
+++ checkcode.py
@@ -7,19 +7,19 @@ import csv
 from datetime import datetime, timedelta
 
 #version control the log files using the date in a MMDDYY format
-today = datetime.now().strftime('%m%d%y') + ".csv"
-yesterday = (datetime.now() - timedelta(days=1)).strftime('%m%d%y') + ".csv"
+new = datetime.now().strftime('%m%d%y') + ".csv"
+old = (datetime.now() - timedelta(days=1)).strftime('%m%d%y') + ".csv"
 
 #path to projects + choose where the logs should be stored
 code_path='/home/vyshah/Projects/codestats/Hilo'
-log_file='/home/vyshah/Projects/codestats/errorlog'
-diff_file='/home/vyshah/Projects/codestats/errordiff'
-dir_ptable_file='/home/vyshah/Projects/codestats/directories_ptable'
-file_ptable_file='/home/vyshah/Projects/codestats/files_ptable'
+log_path='/home/vyshah/Projects/codestats/errorlog'
+diff_path='/home/vyshah/Projects/codestats/errordiff'
+dir_ptable_path='/home/vyshah/Projects/codestats/directories_ptable'
+file_ptable_path='/home/vyshah/Projects/codestats/files_ptable'
 
-#update today's logs
-with open(log_file + today, "w") as err_log:
-    subprocess.call(["cppcheck", "--enable=all", 
+#update new's logs
+with open(log_path + new, "w") as err_log:
+    subprocess.call(["cppcheck", "--enable=all",
     "--template={file},{severity},{id},{message}",
     code_path], stderr=err_log)
 
@@ -31,7 +31,7 @@ dir_names = []
 err_codes = []
 
 #turn the cppcheck output into a readable nested list
-err_log = open(log_file + today, "r")
+err_log = open(log_path + new, "r")
 for num, line in enumerate(err_log, 0):
 
     #each element is a list itself
@@ -59,20 +59,20 @@ dir_ptable = \
         [[0 for i in range(len(err_codes))] for j in range (len(dir_names))]
 
 #fill in pivot tables simultaneously by reading error log
-row = 0
+rownum = 0
 for err in err_table:
-    dir_ptable[dir_names.index(err_table[row][0])] \
-            [err_codes.index(err_table[row][3])] += 1
-    file_ptable[file_names.index(err_table[row][1])] \
-            [err_codes.index(err_table[row][3])] += 1
-    row += 1
+    dir_ptable[dir_names.index(err_table[rownum][0])] \
+            [err_codes.index(err_table[rownum][3])] += 1
+    file_ptable[file_names.index(err_table[rownum][1])] \
+            [err_codes.index(err_table[rownum][3])] += 1
+    rownum += 1
 
 #substitute blank file/dir names for certain errors
 if file_names[0] == '': file_names[0] = "No Particular File"
 if dir_names[0] == '': dir_names[0] = "No Particular Directory"
 
 #convert pivot tables to csv files
-with open(dir_ptable_file + today, 'wb') as csvfile:
+with open(dir_ptable_path + new, 'wb') as csvfile:
     dirwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting = \
             csv.QUOTE_MINIMAL)
     dirwriter.writerow(["Directories/Errors"] +  err_codes)
@@ -80,8 +80,7 @@ with open(dir_ptable_file + today, 'wb') as csvfile:
     for row in dir_ptable:
        dirwriter.writerow([dir_names[rownum]] +  row)
        rownum += 1
-
-with open(file_ptable_file + today, 'wb') as csvfile:
+with open(file_ptable_path + new, 'wb') as csvfile:
     dirwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting = \
             csv.QUOTE_MINIMAL)
     dirwriter.writerow(["Filenames/Errors"] +  err_codes)
